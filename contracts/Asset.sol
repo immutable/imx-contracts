696efd04d869f5ce088b9a41f424d503928e86ab
Samuel Tan <91454198+samueltan-immutable@users.noreply.github.com>
2022-05-06 16:20:42 +1000

    Add function to set the TokenURI
    OpenSea requires the TokenURI to display metadata correctly in their marketplace. Including this function as a standard in our contract example will help minimise issues for customers who use this example contract to deploy a collection and need to add a TokenURI retrospectively.

--- contracts/Asset.sol
+++ contracts/Asset.sol
@@ -5,6 +5,10 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "./Mintable.sol";
 
 contract Asset is ERC721, Mintable {
+
+    // Optional - used for L1-level late reveals, a static string can be used instead (check below)
+    string public baseTokenURI;
+    
     constructor(
         address _owner,
         string memory _name,
@@ -15,8 +19,22 @@ contract Asset is ERC721, Mintable {
     function _mintFor(
         address user,
         uint256 id,
-        bytes memory
+        bytes memory blueprint
     ) internal override {
         _safeMint(user, id);
+        // you may store the blueprint (on-chain metadata) here or implement some logic that relies on the blueprint data passed
+        // below is a bare-minimum implementation of a simple mapping, comment it out if you are not storing on-chain metadata
+        metadata[id] = blueprint;
+    }
+    
+    // overwrite OpenZeppelin's _baseURI to define the base for tokenURI
+    // can be a static value, use a variable if you want the ability to change this (L1 late-reveal)
+    function _baseURI() internal view override returns (string memory) {
+        return baseTokenURI;
+    }
+
+    // optional - change the baseTokenURI for late-reveal purposes
+    function setBaseTokenURI(string memory uri) public onlyOwner {
+        baseTokenURI = uri;
     }
 }
